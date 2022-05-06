9db02d6e7cc4c146f4ea46750797b0f72db78eb8
amitkhan <khan1807111@stud.kuet.ac.bd>
2022-05-07 02:50:03 +0600

    java class is added

---
+++ app/src/androidTest/java/branch/branch_feature.java
@@ -0,0 +1,35 @@
+package branch;
+
+public class branch_feature {
+    String name, id;
+    float gpa;
+    branch_feature(String s,String s1,float f){
+        this.name=s;
+        this.id=s1;
+        this.gpa=f;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
